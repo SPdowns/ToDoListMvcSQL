a8a21ac49cc3198bdb52f7273b098d80ad226c4a
Sean Downs <spdowns@protonmail.com>
2020-07-28 05:26:28 -0700

    finish the SQL lesson and move on to the Entity lesson

--- ToDoListMvcSQL/Models/Item.cs
+++ ToDoListMvcSQL/Models/Item.cs
@@ -6,11 +6,29 @@ namespace ToDoListMvcSQL.Models
   public class Item
   {
     public string Description { get; set; }
-    public int Id { get; }
-    public Item(string description, int id)
-    {
-      Description = description;
-      Id = id;
+    public int Id { get; set; }
+      public Item(string description)
+      {
+        Description = description;
+      }
+      public Item(string description, int id)
+      {
+        Description = description;
+        Id = id;
+      }
+        public override bool Equals(System.Object otherItem)
+      {
+      if (!(otherItem is Item))
+      {
+        return false;
+      }
+      else
+      {
+        Item newItem = (Item)otherItem;
+        bool idEquality = (this.Id == newItem.Id);
+        bool descriptionEquality = (this.Description == newItem.Description);
+        return (idEquality && descriptionEquality);
+      }
     }
     public static List<Item> GetAll()
     {
@@ -47,10 +65,51 @@ namespace ToDoListMvcSQL.Models
         conn.Dispose();
       }
     }
-    public static Item Find(int searchId)
+    public static Item Find(int id)
     {
-      Item placeholderItem = new Item("placeholder item");
-      return placeholderItem;
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM `items` WHERE id = @thisId;";
+      MySqlParameter thisId = new MySqlParameter();
+      thisId.ParameterName = "@thisId";
+      thisId.Value = id;
+      cmd.Parameters.Add(thisId);
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      int itemId = 0;
+      string itemDescription = "";
+      while (rdr.Read())
+      {
+        itemId = rdr.GetInt32(0);
+        itemDescription = rdr.GetString(1);
+      }
+      Item foundItem = new Item(itemDescription, itemId);
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return foundItem;
+    }
+
+      public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO items (description) VALUES (@ItemDescription);";
+      MySqlParameter description = new MySqlParameter();
+      description.ParameterName = "@ItemDescription";
+      description.Value = this.Description;
+      cmd.Parameters.Add(description);    
+      cmd.ExecuteNonQuery();
+      Id = (int) cmd.LastInsertedId;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
     }
   }
 }
